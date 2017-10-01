4ce68917c41fb8e77a0077568d8aa5aba1eb53e8
Samer Makary <samermakary@copticacademy.org>
2017-10-01 03:42:56 +0200

    Exchanged Bootstrap for Angular Material for Scala front-end UI.

--- nhs_backend/scala/src/main/resources/static/nhs/js/nhsConditionsApp.js
+++ nhs_backend/scala/src/main/resources/static/nhs/js/nhsConditionsApp.js
@@ -1,2 +1,2 @@
 // Define the `nhsConditionsApp` module
-var nhsConditionsApp = angular.module('nhsConditionsApp', [ 'ngResource' ]);
+var nhsConditionsApp = angular.module('nhsConditionsApp', [ 'ngResource', 'ngMaterial' ]);
